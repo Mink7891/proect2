8df82923d9bdee5982839a36d011b176c01878e9
oleg <shelubskiimisha@gmail.com>
2020-01-24 21:41:42 +0300

    сделал удаление кнопок

--- proect3.py
+++ proect3.py
@@ -2,8 +2,16 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 import random
 
 
-
-
+buttons=[]
+def button_clicked(button):
+    button.setParent(None)
+    print(button.column, button.row)
+    text1 = button.text()
+    i = button.row
+    j = button.column
+    text_up=buttons[i-1][j].text()
+    if text1==text_up:
+        buttons[i-1][j].setParent(None)
 def create_button(x, y, window):
     c=random.randint(6,9)
     button=QPushButton()
@@ -11,6 +19,7 @@ def create_button(x, y, window):
     button.move(x,y)
     button.setText(str(c))
     window.layout().addWidget(button)
+    button.clicked.connect(lambda: button_clicked(button))
     if c==6:
         button.setStyleSheet('QPushButton {background-color: red}')
     elif c==7:
@@ -29,11 +38,16 @@ def create_button(x, y, window):
         button.setStyleSheet('QPushButton {background-color: gold}')
     elif c==5:
         button.setStyleSheet('QPushButton {background-color: cyan}')
+    return button
 
-def crate_button_rov(y,window):
+
+def crate_button_rov(y,window, coluns, k):
     x=40
-    for i in range(10):
-        create_button(x, y, window)
+    for j in range(10):
+        button=create_button(x,y, window)
+        coluns.append(button)
+        button.column=j
+        button.row=k
         x=x+30
 
 
@@ -45,10 +59,13 @@ if __name__ == "__main__":
 
     y=40
     for i in range(10):
-        crate_button_rov(y,window)
+        coluns=[]
+        buttons.append(coluns)
+        crate_button_rov(y,window, coluns, i)
         y=y+30
 
 
+    # print(buttons)
 
     window.show()
     app.exec()
