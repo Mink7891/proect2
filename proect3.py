d4685613eb0a11233321d0a412e4bb9c3fb48de4
mink7891 <example@example.ru>
2020-02-03 21:29:57 +0300

    03.02.20

--- proect3.py
+++ proect3.py
@@ -3,31 +3,58 @@ import random
 
 
 buttons=[]
+n=10
+
 def button_clicked(button):
     # button.setParent(None)
     print(button.column, button.row)
-    text1 = button.text()
+    # text1 = button.text()
     i = button.row
     j = button.column
-    if i!=0:
+    walk(i,j)
+    # if i!=0:
+    #     text_up=buttons[i-1][j].text()
+    #     if text1==text_up:
+    #         buttons[i-1][j].setParent(None)
+    # if i!=len(buttons)-1:
+    #     text_down=buttons[i+1][j].text()
+    #     if text1==text_down:
+    #         buttons[i+1][j].setParent(None)
+    # if j!=0:
+    #     text_left=buttons[i][j-1].text()
+    #     if text1==text_left:
+    #         buttons[i][j-1].setParent(None)
+    # if j!=len(buttons)-1:
+    #     text_right=buttons[i][j+1].text()
+    #     if text1==text_right:
+    #         buttons[i][j+1].setParent(None)
+    window.layout().addWidget(button)
+
+def walk(i, j):
+    global n
+    button=buttons[i][j]
+    parent = button.parent()
+    button.setParent(None)
+    text1 = button.text()
+
+    if parent is None:
+        return
+    if i>0:
         text_up=buttons[i-1][j].text()
         if text1==text_up:
-            buttons[i-1][j].setParent(None)
-    if i!=len(buttons)-1:
-        text_down=buttons[i+1][j].text()
-        if text1==text_down:
-            buttons[i+1][j].setParent(None)
-    if j!=0:
+            walk(i-1,j)
+    if j>0:
         text_left=buttons[i][j-1].text()
         if text1==text_left:
-            buttons[i][j-1].setParent(None)
-    if j!=len(buttons)-1:
+            walk(i,j-1)
+    if i<n-1:
+        text_down=buttons[i+1][j].text()
+        if text1==text_down:
+         walk(i+1,j)
+    if j<n-1:
         text_right=buttons[i][j+1].text()
         if text1==text_right:
-            buttons[i][j+1].setParent(None)
-
-
-
+         walk(i,j+1)
 
 
 
@@ -80,7 +107,7 @@ if __name__ == "__main__":
     window.setFixedSize(400, 400)
 
     y=40
-    for i in range(10):
+    for i in range(n):
         coluns=[]
         buttons.append(coluns)
         crate_button_rov(y,window, coluns, i)
